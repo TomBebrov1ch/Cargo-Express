2a36c71d7e6776c15f0216d14e97b4edb8179d42
fwefew123 <artem.andreev200012@mail.ru>
2024-01-11 13:57:08 +0600

    version 1.0.0

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,37 @@ import "../Header/header.scss";
 
 const Header = (props) => {
   return (
-    <header className="header">
-      <nav className="header__items">
-        <img src={logo} alt="logo" className="header__logo" />
-        <Hamburger
-          className="header__hamburger"
-          color="#025A42"
-          toggled={props.isOpen}
-          toggle={props.toggleMenu}
-        />
-      </nav>
-    </header>
+    <>
+      <section className="mob-screen">
+        <header className="header">
+          <nav className="header__items">
+            <img src={logo} alt="logo" className="header__logo" />
+            <Hamburger
+              className="header__hamburger"
+              color="#025A42"
+              toggled={props.isOpen}
+              toggle={props.toggleMenu}
+            />
+          </nav>
+        </header>
+      </section>
+      <section className="pc-screen">
+        <header className="header-pc">
+          <nav className="header-pc__content">
+            <img src={logo} alt="logo" className="header-pc__logo" />
+            <div className="header-pc__content__text-s">
+              <p className="header-pc__content__text">Главная</p>
+              <p className="header-pc__content__text">О нас</p>
+              <p className="header-pc__content__text">Наши вагоны</p>
+              <p className="header-pc__content__text">Контакты</p>
+            </div>
+            <div className="header-pc__content__bnt">
+              <button className="header-pc__content__button">Связаться с нами</button>
+            </div>
+          </nav>
+        </header>
+      </section>
+    </>
   );
 };
 
--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Fade } from "react-awesome-reveal";
 import "./styles/mainscreen.scss";
 import MainTrain from "../../assets/main_train.webp";
+import TrainPc from '../../assets/main_train-pc.webp'
 
 import Header from "../../components/Header/Header.jsx";
 import Menu from "../../components/Menu/Menu.jsx";
@@ -27,39 +28,42 @@ const MainScreen = () => {
   }, [isOpen]);
   return (
     <>
-      <main className="main">
-        <Header isOpen={isOpen} toggleMenu={toggleMenu} />
-        {isOpen && <Menu isOpen={isOpen} toggleMenu={toggleMenu} />}
-        <div className="main__content">
-          <Fade direction="up" triggerOnce>
-            <h1 className="main__first">
-              <span className="main__span">Cargo Express PV – </span>Эффективная
-              и надёжная доставка грузов
-            </h1>
-          </Fade>
-          <Fade direction="up" delay={200} triggerOnce>
-            <p className="main__second">
-              Будьте уверены в том, что ваш груз будет доставлен вовремя и в
-              целости с "Cargo Express PV". Наша компания предоставляет
-              профессиональные услуги по перевозке товаров в вагонах по всей
-              территории Казахстана, гарантируя высокую надежность и
-              безопасность.
-            </p>
-          </Fade>
-          <Fade direction="right" delay={300} triggerOnce>
-            <ButtonOur text={"Наши вагоны"} showImage={true} />
-          </Fade>
-          <Fade direction="left" delay={400} triggerOnce>
-            <ButtonMore text="Подробнее" className={"simple"} />
-          </Fade>
-        </div>
-        <Fade direction="up">
+      <section className="mob-screen-main">
+        <main className="main">
+          <Header isOpen={isOpen} toggleMenu={toggleMenu} />
+          {isOpen && <Menu isOpen={isOpen} toggleMenu={toggleMenu} />}
+          <div className="main__content">
+            <Fade direction="up" triggerOnce>
+              <h1 className="main__first">
+                <span className="main__span">Cargo Express PV – </span>Эффективная
+                и надёжная доставка грузов
+              </h1>
+            </Fade>
+            <Fade direction="up" delay={200} triggerOnce>
+              <p className="main__second">
+                Будьте уверены в том, что ваш груз будет доставлен вовремя и в
+                целости с "Cargo Express PV". Наша компания предоставляет
+                профессиональные услуги по перевозке товаров в вагонах по всей
+                территории Казахстана, гарантируя высокую надежность и
+                безопасность.
+              </p>
+            </Fade>
+            <div className="main__buttons">
+              <Fade direction="right" delay={300} triggerOnce>
+                <ButtonOur text={"Наши вагоны"} showImage={true} />
+              </Fade>
+              <Fade direction="left" delay={400} triggerOnce>
+                <ButtonMore text="Подробнее" className={"simple"} />
+              </Fade>
+            </div>
+          </div>
+          <img src={TrainPc} alt="train" className="main__train-pc" />
           <img src={MainTrain} alt="train" className="main__train" />
-        </Fade>
-        <div className="main__calc">
-          <Calculator />
-        </div>
-      </main>
+          <div className="main__calc">
+            <Calculator />
+          </div>
+        </main>
+      </section>
     </>
   );
 };
